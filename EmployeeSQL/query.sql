3b6e5bd7041779e350cb55c124839eaadef64b83
Jordan Gilmartin <jordan.gilmartin@yahoo.com>
2021-01-22 17:36:19 -0500

    query #2

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -8,3 +8,15 @@ salaries.salary
 from employees
 inner join salaries on
 employees.emp_no = salaries.emp_no;
+
+
+
+-- List first name, last name, and hire date for employees who were hired in 1986
+select 
+employees.first_name,
+employees.last_name,
+employees.hire_date
+from employees
+where hire_date >= '1986-01-01'
+and hire_date <= '1986-12-31'
+order by 3;
