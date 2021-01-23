37823b4a18165108ba10c43baf2c8be07a4bab3d
Jordan Gilmartin <jordan.gilmartin@yahoo.com>
2021-01-22 21:48:14 -0500

    query #4

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -36,3 +36,19 @@ join departments on
 dept_manager.dept_no = departments.dept_no
 join employees on
 dept_manager.emp_no = employees.emp_no;
+
+
+
+-- List the department of each employee with the following information:
+-- employee number, last name, first name, and department name
+select 
+employees.emp_no,
+employees.last_name,
+employees.first_name,
+departments.dept_name
+from employees
+join dept_emp on
+employees.emp_no = dept_emp.emp_no
+join departments on
+departments.dept_no = dept_emp.dept_no
+order by 4;
