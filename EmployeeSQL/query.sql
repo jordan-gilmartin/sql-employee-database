ed971b257bfb0fee64f068dd89fc6511b18aed09
Jordan Gilmartin <jordan.gilmartin@yahoo.com>
2021-01-22 21:55:18 -0500

    query #5

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -1,4 +1,4 @@
--- List the following details of each employee: employee number, last name, first name, sex and salary
+-- 1. List the following details of each employee: employee number, last name, first name, sex and salary
 select 
 employees.emp_no,
 employees.last_name,
@@ -11,7 +11,7 @@ employees.emp_no = salaries.emp_no;
 
 
 
--- List first name, last name, and hire date for employees who were hired in 1986
+-- 2. List first name, last name, and hire date for employees who were hired in 1986
 select 
 employees.first_name,
 employees.last_name,
@@ -23,7 +23,7 @@ order by 3;
 
 
 
--- List the manager of each department with the following information:
+-- 3. List the manager of each department with the following information:
 -- department number, department name, the manager's employee number, last name, first name
 select 
 dept_manager.dept_no,
@@ -39,7 +39,7 @@ dept_manager.emp_no = employees.emp_no;
 
 
 
--- List the department of each employee with the following information:
+-- 4. List the department of each employee with the following information:
 -- employee number, last name, first name, and department name
 select 
 employees.emp_no,
@@ -52,3 +52,13 @@ employees.emp_no = dept_emp.emp_no
 join departments on
 departments.dept_no = dept_emp.dept_no
 order by 4;
+
+
+
+-- 5. List first name, last name, and sex for employees 
+-- whose first name is "Hercules" and last names begin with "B".  
+select first_name, last_name, sex
+from employees
+where first_name = 'Hercules'
+and last_name like 'B%'
+order by 2;
